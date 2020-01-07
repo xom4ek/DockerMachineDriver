b39ec720179611b7d7518e00d12d45456866b1be
Artem Goncharov <Artem.goncharov@gmail.com>
2020-01-07 15:58:16 +0100

    Update main.go

--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"github.com/docker/machine/libmachine/drivers/plugin"
-	"github.com/huawei/DockerMachineDriver4OTC/otc"
+	"github.com/opentelekomcloud/DockerMachineDriver/otc"
 )
 
 func main() {
